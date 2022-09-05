6eb794846881ecd63de95bca7280e9c6a1032ede
Elia Biraschi <elia.biraschi@traxretail.com>
2022-09-05 08:59:35 +0200

    feat: jump to now button
    - added button to epg with logic and style to autoscroll to now

--- src/Containers/EPG/EPG.tsx
+++ src/Containers/EPG/EPG.tsx
@@ -5,6 +5,9 @@ import Channels from '../../Components/Channels/Channels';
 import NowMarker from '../../Components/NowMarker/NowMarker';
 import TimeLine from '../../Components/TimeLine/TimeLine';
 import Program from '../../Components/Program/Program';
+import {
+  getDistanceFromMidnight,
+} from '../../Modules/SpaceTime';
 import './EPG.css';
 
 interface Props {
@@ -14,12 +17,21 @@ interface Props {
 const daysRange = [-3,-2,-1,0,1,2,3];
 
 function EPG(props: Props) {
+  const jumpToNow = () => {
+    const allPrograms = document.getElementById('all-programs');
+    if (allPrograms) {
+      allPrograms.scrollLeft = 
+        (getDistanceFromMidnight() * parseFloat(getComputedStyle(document.documentElement).fontSize))
+        - ((window.screen.width / window.devicePixelRatio) / 2);
+    }
+  };
+
   return (
     <div id="EPG">
       <Calendar range={daysRange} />
       <div className="programs-list">
         <Channels channels={props.data.channels} />
-        <div className="all-programs">
+        <div className="all-programs" id="all-programs">
           <NowMarker />
           <TimeLine />
           {
@@ -27,12 +39,13 @@ function EPG(props: Props) {
               return (
                 <div key={channel.id} className="programs-of-the-day">
                   {channel.schedules.map((program, index) => {
-                    return (<Program program={program} isLast={index === (channel.schedules.length - 1)} />);
+                    return (<Program key={program.id + index} program={program} isLast={index === (channel.schedules.length - 1)} />);
                   })}
                 </div>
               )
             })
           }
+          <button onClick={jumpToNow} className="jump-to-now">NOW</button>
         </div>
       </div>
     </div>
