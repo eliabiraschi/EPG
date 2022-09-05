bac5f79be11920630d6b262a2d0d55599461e46e
Elia Biraschi <elia.biraschi@traxretail.com>
2022-09-05 12:24:59 +0200

    feat: now-marker autrefresh
    - added interval to refresh the now-marker position every minute

--- src/Components/NowMarker/NowMarker.tsx
+++ src/Components/NowMarker/NowMarker.tsx
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   getDistanceFromMidnight,
 } from '../../Modules/SpaceTime';
 import './NowMarker.css';
 
 function NowMarker() {
+  const [refreshCount, setRefreshCount] = useState(0);
+  setInterval(() => {
+    setRefreshCount(refreshCount + 1);
+    console.log('Refreshed marker', refreshCount);
+  }, 60 * 1000);
+
   return (
     <div
+      x-data-refesh-count={refreshCount}
       className="now-marker"
       style={{
         left: `${getDistanceFromMidnight()}rem`,
